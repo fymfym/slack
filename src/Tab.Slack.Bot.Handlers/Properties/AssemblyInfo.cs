293b9b9f19b1000cdc9c3f75ff785953230745f3
Rob Ladbrook <rlgithub@slyfx.com>
2015-05-18 11:45:22 +0100

    Submitted initial alpha to nuget

--- src/Tab.Slack.Bot.Handlers/Properties/AssemblyInfo.cs
+++ src/Tab.Slack.Bot.Handlers/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tab.SlackBot.CoreHandlers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Slack Bot Handlers")]
+[assembly: AssemblyDescription("Set of common/core handlers for Slack Bot")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tab.SlackBot.CoreHandlers")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Rob Ladbrook (The App Business)")]
+[assembly: AssemblyProduct("Tab.Slack.Bot.Handlers")]
+[assembly: AssemblyCopyright("Copyright © The App Business 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1")]
--- src/Tab.Slack.WebApi/Properties/AssemblyInfo.cs
+++ src/Tab.Slack.WebApi/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tab.Slack.WebApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Slack Web API Client Library")]
+[assembly: AssemblyDescription("A simple library which allows you to make calls to Slack via it's Web API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rob Ladbrook (The App Business)")]
 [assembly: AssemblyProduct("Tab.Slack.WebApi")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © The App Business 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1")]
