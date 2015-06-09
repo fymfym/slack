4a16b94961a6c79070899f66cee9936e69ae51ac
Rob Ladbrook <rlgithub@slyfx.com>
2015-06-09 15:05:29 +0100

    Added core handler to deal with tracking bot states

---
+++ src/Tab.Slack.Bot/CoreHandlers/BotHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tab.Slack.Common.Model.Events;
+using Tab.Slack.Common.Model.Events.Messages;
+using Tab.Slack.Bot.Integration;
+using Tab.Slack.Common.Model;
+
+namespace Tab.Slack.Bot.CoreHandlers
+{
+    [Export(typeof(IMessageHandler))]
+    public class BotHandler : MessageHandlerBase, IMessageHandler
+    {
+        public bool CanHandle(EventMessageBase message)
+        {
+            return message.IsOneOf(EventType.BotAdded, EventType.BotChanged);
+        }
+
+        public Task<ProcessingChainResult> HandleMessageAsync(EventMessageBase message)
+        {
+            var botModel = (message as BotAdded)?.Bot ?? (message as BotChanged)?.Bot;
+
+            if (botModel != null)
+            {
+                var existingBotIndex = base.BotState.Bots.FindIndex(b => b.Id == botModel.Id);
+
+                if (existingBotIndex >= 0)
+                    base.BotState.Bots.RemoveAt(existingBotIndex);
+
+                base.BotState.Bots.Add(botModel);
+            }
+
+            return Task.FromResult(ProcessingChainResult.Continue);
+        }
+    }
+}
---
+++ test/Tab.Slack.Bot.Tests/CoreHandlers/BotHandlerTests.cs
@@ -0,0 +1,116 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tab.Slack.Common.Model;
+using Tab.Slack.Common.Model.Events;
+using Tab.Slack.Common.Model.Events.Messages;
+using Xunit;
+
+namespace Tab.Slack.Bot.CoreHandlers.Tests
+{
+    public class BotHandlerTests
+    {
+        [Fact]
+        public void CanHandleBotMessages()
+        {
+            var handler = new BotHandler();
+
+            var result = handler.CanHandle(new BotChanged { Type = EventType.BotChanged });
+            Assert.True(result);
+
+            result = handler.CanHandle(new BotAdded { Type = EventType.BotAdded });
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void HandlesValidBotAdded()
+        {
+            var bots = new List<BotModel>
+            {
+                new BotModel { Id = "BOTID1" },
+                new BotModel { Id = "BOTID2" }
+            };
+
+            var mockState = new Mock<IBotState>();
+            mockState.Setup(s => s.Bots)
+                     .Returns(bots)
+                     .Verifiable();
+
+            var message = new BotAdded
+            {
+                Type = EventType.BotAdded,
+                Bot = new BotModel { Id = "BOTID3" }
+            };
+
+            var handler = new BotHandler();
+            handler.BotState = mockState.Object;
+
+            var result = await handler.HandleMessageAsync(message);
+
+            mockState.Verify();
+            Assert.Equal(3, bots.Count);
+            Assert.Equal("BOTID3", bots.Last().Id);
+        }
+
+        [Fact]
+        public async void HandlesValidBotChanged()
+        {
+            var bots = new List<BotModel>
+            {
+                new BotModel { Id = "BOTID1", Name = "NameA" },
+                new BotModel { Id = "BOTID2" }
+            };
+
+            var mockState = new Mock<IBotState>();
+            mockState.Setup(s => s.Bots)
+                     .Returns(bots)
+                     .Verifiable();
+
+            var message = new BotChanged
+            {
+                Type = EventType.BotChanged,
+                Bot = new BotModel { Id = "BOTID1", Name = "NameB" }
+            };
+
+            var handler = new BotHandler();
+            handler.BotState = mockState.Object;
+
+            var result = await handler.HandleMessageAsync(message);
+
+            mockState.Verify();
+            Assert.Equal(2, bots.Count);
+            Assert.Equal("NameB", bots.First(b => b.Id == "BOTID1").Name);
+        }
+
+        [Fact]
+        public async void AddsMissingChangedBot()
+        {
+            var bots = new List<BotModel>
+            {
+                new BotModel { Id = "BOTID2" }
+            };
+
+            var mockState = new Mock<IBotState>();
+            mockState.Setup(s => s.Bots)
+                     .Returns(bots)
+                     .Verifiable();
+
+            var message = new BotChanged
+            {
+                Type = EventType.BotChanged,
+                Bot = new BotModel { Id = "BOTID1", Name = "NameB" }
+            };
+
+            var handler = new BotHandler();
+            handler.BotState = mockState.Object;
+
+            var result = await handler.HandleMessageAsync(message);
+
+            mockState.Verify();
+            Assert.Equal(2, bots.Count);
+            Assert.Equal("NameB", bots.First(b => b.Id == "BOTID1").Name);
+        }
+    }
+}
