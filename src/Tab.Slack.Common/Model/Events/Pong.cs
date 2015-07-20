1bdbb712ed1e179ad2c6b9bfbec2c4d72806c878
Rob Ladbrook <rlgithub@slyfx.com>
2015-07-20 20:25:30 +0100

    Missing attributes on Pong event (although they're not meant to exist so not sure what Slack is playing at)

--- src/Tab.Slack.Common/Model/Events/Pong.cs
+++ src/Tab.Slack.Common/Model/Events/Pong.cs
@@ -9,5 +9,7 @@ namespace Tab.Slack.Common.Model.Events
     {
         public string ReplyTo { get; set; }
         public string Time { get; set; }
+        public string Channel { get; set; }
+        public string Text { get; set; }
     }
 }
